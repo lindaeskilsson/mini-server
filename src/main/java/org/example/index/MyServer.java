ce38fe0edaf6a33ee87265960367a25393e2360e
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-02-12 11:22:27 +0100

    Här är en bra commit-message (kort men tydlig enligt vanlig standard):

---
+++ src/main/java/org/example/index/MyServer.java
@@ -0,0 +1,52 @@
+package org.example.index;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServer {
+     static void main() throws IOException {
+
+        System.out.println("Starting server.....");
+
+        // Starta server (bind port: 4000)
+        ServerSocket server = new ServerSocket(4000);
+        System.out.println("Server started. Listening to port 4000");
+
+        // While true -> accept clients
+        while (true){
+            Socket client = server.accept();
+            System.out.println("Client connected");
+            handleClient(client);
+        }
+        //respond
+        // Close socket
+    }
+
+    // for each client:
+    private static void handleClient(Socket client) {
+        try(
+                InputStream in = client.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                OutputStream out = client.getOutputStream();
+                PrintWriter writer = new PrintWriter(out)
+                ){
+            //Read message, inputStream
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                System.out.println(line);
+            }
+            //respond
+            writer.print("HTTP/1.1 200 OK\r\n");
+            writer.print("Content-Type: text/plain\r\n");
+            writer.print("\r\n");
+            writer.print("Hello Linda");
+            writer.flush();
+
+            client.close();}
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        }
+}
